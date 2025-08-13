c28ecae291ac70e27cc7c08ee61bb6d076a9f064
Hardik-kahar <dev2-uidai-gil@gujarat.gov.in>
2025-08-13 18:09:43 +0530

    service setup

---
+++ order-service/src/main/java/com/ecommerce/order_service/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.ecommerce.order_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @GetMapping()
+    public String getOrders() {
+        return "List of Orders";
+    }
+}
