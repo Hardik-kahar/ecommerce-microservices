b7fb56da857539b1b675c8437e525b7433379820
Hardik-kahar <kahardik1997@gmail.com>
2025-08-25 22:58:24 +0530

    services connect with config server

--- payment-service/src/main/java/com/ecommerce/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/controller/PaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("payment")
 public class PaymentController {
 
-    @GetMapping()
+    @GetMapping
     public String getPayments() {
         return "List of payments";
     }
--- product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.ecommerce.product_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/products")
+    @GetMapping
     public String getProducts() {
         return "List of Products";
     }
+
 }
